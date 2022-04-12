87c11e982228cd4180d764ed4fe35eea11bec043
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-12 11:34:50 +0900

    migrate posts to React 18

--- posts/src/index.tsx
+++ posts/src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.querySelector('#root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOMClient.createRoot(rootElement)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
