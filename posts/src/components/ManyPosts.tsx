d03ca2b3fbc888913c521afa09a1755836709265
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-13 14:04:29 +0900

    32 prefetching on link hover

--- posts/src/components/ManyPosts.tsx
+++ posts/src/components/ManyPosts.tsx
@@ -1,51 +1,48 @@
 import axios from 'axios';
 import { useReducer } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { Post } from '../types';
 import { Link } from 'react-router-dom';
+import { getSinglePost } from './SinglePost';
 
 export const fetchPosts = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const posts: Post[] = await axios
     .get('https://jsonplaceholder.typicode.com/posts')
     .then((res) => res.data);
-  
+
   // // iterate over posts and optimistically push into query cache
   // posts.forEach((post) => {
   //   queryClient.setQueryData(['post', post.id], post);
   // });
 
   return posts;
-}
+};
 
 function ManyPosts() {
-  const [count, increment] = useReducer(d => d + 1, 0);
+  const queryClient = useQueryClient();
+  const [count, increment] = useReducer((d) => d + 1, 0);
 
-  const postsQuery = useQuery<Post[], Error>(
-    'posts',
-    fetchPosts,
-    {
-      cacheTime: 60 * 60 * 1000,
-      onSuccess: (data) => {
-        increment();
-      },
-      // onError: (error) => {}
-      // onSettled: (da ta, error) => {} runs on both success and error
-      // these run everytime the query is run, ie if we use it in 4 components
-      // then it gets run 4 times
-      
-      // refetchInterval: 1000 * 5, // refetch after 5 seconds while focus
-      // refetchIntervalInBackground: true, // also refetch when tab is in background
-      staleTime: Infinity,
-    }
-  );
+  const postsQuery = useQuery<Post[], Error>('posts', fetchPosts, {
+    cacheTime: 60 * 60 * 1000,
+    onSuccess: (data) => {
+      increment();
+    },
+    // onError: (error) => {}
+    // onSettled: (da ta, error) => {} runs on both success and error
+    // these run everytime the query is run, ie if we use it in 4 components
+    // then it gets run 4 times
 
-  
+    // refetchInterval: 1000 * 5, // refetch after 5 seconds while focus
+    // refetchIntervalInBackground: true, // also refetch when tab is in background
+    staleTime: Infinity,
+  });
+
   return (
     <div>
       <h1>Posts{postsQuery.isFetching && '...'}</h1>
       <h4>Fetched {count} times</h4>
-      
+
       <div>
         {postsQuery.isLoading || postsQuery.isError || postsQuery.isIdle ? (
           <div>'Loading posts...'</div>
@@ -54,10 +51,18 @@ function ManyPosts() {
             <ul>
               {postsQuery.data.map((post) => {
                 return (
-                  <li key={post.id}>
-                    <Link to={`/post/${post.id}`}>
-                      {post.title}
-                    </Link>
+                  <li
+                    key={post.id}
+                    onMouseEnter={() =>
+                      queryClient.prefetchQuery(
+                        ['post', String(post.id)],
+                        () => {
+                          return getSinglePost(String(post.id));
+                        }
+                      )
+                    }
+                  >
+                    <Link to={`/post/${post.id}`}>{post.title}</Link>
                   </li>
                 );
               })}
--- posts/src/components/SinglePost.tsx
+++ posts/src/components/SinglePost.tsx
@@ -1,38 +1,34 @@
 import axios from 'axios';
-import {  useQuery } from 'react-query';
+import { useQuery } from 'react-query';
 import { Post } from '../types';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
 
+export const getSinglePost = async (id: string) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const post: Post = await axios
+    .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((res) => res.data);
+  return post;
+};
+
 function SinglePost() {
   const { id } = useParams();
 
-  const postQuery = useQuery<Post, Error>(
-    ['post', id],
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const post = await axios
-        .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        .then((res) => res.data);
-      return post
-    },
-    {
-      // // GETTING initialData
-      // // we already have the post data from the 'posts' query, so we can use that
-      // // until the 'post' query comes in
-      // // this is a 'pull' sort of operation
-      // initialData: () => {
-      //   const data = queryClient.getQueryData<Post[]>('posts');
-      //   return data ? data.find(post => post.id === postId) : undefined;
-      // },
-    }
-  );
+  const postQuery = useQuery<Post, Error>(['post', id], () => getSinglePost(id || ''), {
+    // // GETTING initialData
+    // // we already have the post data from the 'posts' query, so we can use that
+    // // until the 'post' query comes in
+    // // this is a 'pull' sort of operation
+    // initialData: () => {
+    //   const data = queryClient.getQueryData<Post[]>('posts');
+    //   return data ? data.find(post => post.id === postId) : undefined;
+    // },
+  });
 
   return (
     <div>
-      <Link to="/">
-        back
-      </Link>
+      <Link to='/'>back</Link>
       {postQuery.isLoading ? (
         'Loading...'
       ) : (
