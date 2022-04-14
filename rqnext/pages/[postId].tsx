3eda8b0a65dede64fbd57375924a0cf5954e72d6
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-14 15:13:27 +0900

    Implement SSR in [postId].tsx

--- rqnext/pages/[postId].tsx
+++ rqnext/pages/[postId].tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import IPost from '../types/Post';
+import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
 
 const fetchPost = async (id: string) => {
   await new Promise((r) => setTimeout(r, 500));
@@ -12,12 +13,36 @@ const fetchPost = async (id: string) => {
     .then((res) => res.data);
 };
 
-export default function Post() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = (context.params![0] as string) || '1';
+  const post = await fetchPost(id);
+
+  return {
+    props: { post },
+  };
+};
+
+interface PageProps {
+  post: IPost;
+}
+
+const Post: FC<PageProps> = (props: PageProps) => {
   let {
     query: { postId },
   } = useRouter();
 
-  const postQuery = useQuery<IPost>(['post', postId], () => fetchPost(String(postId)));
+  const postQuery = useQuery<IPost>(
+    ['post', postId],
+    () => {
+      return fetchPost(String(postId));
+    },
+    {
+      initialData: props.post,
+      refetchOnMount: true,
+    }
+  );
 
   return (
     <>
@@ -25,14 +50,18 @@ export default function Post() {
         <span>Loading...</span>
       ) : (
         <div>
-          <Link href="/">
+          <Link href='/'>
             <a>Back</a>
           </Link>
           <h3>{postQuery.data?.title}</h3>
-          <p><small>Post ID: {postQuery.data?.id}</small></p>
+          <p>
+            <small>Post ID: {postQuery.data?.id}</small>
+          </p>
           <p>{postQuery.data?.body}</p>
         </div>
       )}
     </>
-  )
-}
+  );
+};
+
+export default Post;
